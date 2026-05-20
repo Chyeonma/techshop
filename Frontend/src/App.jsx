8f0056c32a9e8514e3fe550255450150b23e4f8e
nguyenvanquyen <nguyen.van.quyen.p3t@gmail.com>
2026-05-21 00:11:27 +0700

    feat: create separate pages for each product

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -8,6 +8,7 @@ import PhonePage from './pages/PhonePage'
 import LaptopPage from './pages/LaptopPage'
 import AccessoryPage from './pages/AccessoryPage'
 import BrandSeriesPage from './pages/BrandSeriesPage'
+import ProductDetailPage from './pages/ProductDetailPage'
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
           <Route path="/laptop" element={<LaptopPage />} />
           <Route path="/laptop/:brand" element={<BrandSeriesPage type="laptop" />} />
           <Route path="/phu-kien" element={<AccessoryPage />} />
+          <Route path="/san-pham/:id" element={<ProductDetailPage />} />
         </Routes>
       </main>
       <Footer />
