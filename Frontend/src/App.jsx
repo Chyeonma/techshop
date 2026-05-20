d8e37a2791246718933e801fc1daf56dc8ee816d
nguyenvanquyen <nguyen.van.quyen.p3t@gmail.com>
2026-05-20 20:43:26 +0700

    feat: Add manufacturer brand pages

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -7,6 +7,7 @@ import HomePage from './pages/HomePage'
 import PhonePage from './pages/PhonePage'
 import LaptopPage from './pages/LaptopPage'
 import AccessoryPage from './pages/AccessoryPage'
+import BrandSeriesPage from './pages/BrandSeriesPage'
 
 function App() {
   return (
@@ -17,7 +18,9 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/dien-thoai" element={<PhonePage />} />
+          <Route path="/dien-thoai/:brand" element={<BrandSeriesPage type="phone" />} />
           <Route path="/laptop" element={<LaptopPage />} />
+          <Route path="/laptop/:brand" element={<BrandSeriesPage type="laptop" />} />
           <Route path="/phu-kien" element={<AccessoryPage />} />
         </Routes>
       </main>
